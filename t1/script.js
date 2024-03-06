502add8f6877ef48dd378e4094da5ece979fe881
Gunel <159937480+GunelShirvanzada@users.noreply.github.com>
2024-03-06 23:57:42 +0400

    Add files via upload

--- t1/script.js
+++ t1/script.js
@@ -57,15 +57,223 @@
 // }
 // hasil([12,2,3,4,5])
 
-// ? meyve------------------------------------------------------
+// ! meyve------------------------------------------------------
 
-function fruit(all) {
-    let name = ['apple', 'banan'];
-    let newName = '';
-    for (let i = name.length - 1; i >= 0; i--) {
-        newName = newName + name[i];
+// function fruit(all) {
+//     let name = ['apple', 'banan'];
+//     let newName = '';
+//     for (let i = name.length - 1; i >= 0; i--) {
+//         newName = newName + name[i];
+//     }
+//     console.log(newName);
+//     return newName
+// }
+// fruit()
+
+
+// ? negativ----------------------------------------------------
+
+// function control(){
+//     let num = +prompt('Bir ədəd yazın');
+//     if(num<0){
+//         alert('Negativ');
+//     } else if(num>0){
+//         alert('Pozitiv');
+//     }
+// }
+// control()
+
+// ? 2eded------------------------------------------------------
+
+// function num (){
+//     let one=+prompt('1.ci rəqəmi yazın.');
+//     let two=+prompt('2.ci rəqəmi yazın.');
+//     let final = (one+two)/2;
+//     alert(final);
+// }
+// num()
+
+// ? 2str-------------------------------------------------------
+
+// function str (){
+//     let one=prompt('1.ci sözü yazın.');
+//     let two=prompt('2.ci sözü yazın.');
+//     let final = (one+two);
+//     alert(final);
+// }
+// str()
+
+// ! 3eded------------------------------------------------------
+
+// ? book-------------------------------------------------------
+
+// function book() {
+//     let books = [
+//         {
+//             author: "J.K. Rowling",
+//             bookName: "Harry Potter and the Sorcerer's Stone",
+//             bestseller: true,
+//             genre: "Fantasy",
+//         },
+//         {
+//             author: "George Orwell",
+//             bookName: "1984",
+//             bestseller: false,
+//             genre: "Dystopian",
+//         },
+//         {
+//             author: "J.R.R. Tolkien",
+//             bookName: "The Hobbit",
+//             bestseller: true,
+//             genre: "Fantasy",
+//         },
+//         {
+//             author: "Jane Austen",
+//             bookName: "Pride and Prejudice",
+//             bestseller: false,
+//             genre: "Classic",
+//         },
+//         {
+//             author: "Harper Lee",
+//             bookName: "To Kill a Mockingbird",
+//             bestseller: true,
+//             genre: "Fiction",
+//         },
+//         {
+//             author: "F. Scott Fitzgerald",
+//             bookName: "The Great Gatsby",
+//             bestseller: false,
+//             genre: "Classic",
+//         },
+//         {
+//             author: "Agatha Christie",
+//             bookName: "Murder on the Orient Express",
+//             bestseller: true,
+//             genre: "Mystery",
+//         },
+//         {
+//             author: "Ernest Hemingway",
+//             bookName: "The Old Man and the Sea",
+//             bestseller: false,
+//             genre: "Fiction",
+//         },
+//         {
+//             author: "Jules Verne",
+//             bookName: "Twenty Thousand Leagues Under the Sea",
+//             bestseller: true,
+//             genre: "Adventure",
+//         },
+//         {
+//             author: "Arthur Conan Doyle",
+//             bookName: "Sherlock Holmes",
+//             bestseller: false,
+//             genre: "Mystery",
+//         },
+//     ];
+//     console.log('Bestseller--------------------------------')
+//     for (i = 0; i < books.length; i++) {
+//         if (books[i].bestseller === true) {
+//             console.log(books[i].bookName);
+//         }
+//     }
+//     console.log('Fantasy-----------------------------------')
+//     for(i = 0; i < books.length; i++){
+//         if (books[i].genre === 'Fantasy'){
+//             console.log(books[i].bookName + ": " + books[i].author);
+//         }
+//     }
+//     console.log('Fantasy and Bestseller--------------------')
+//     for(i = 0; i < books.length; i++){
+//         if (books[i].genre === 'Fantasy'&& books[i].bestseller === true){
+//             console.log(books[i].bookName);
+//         }
+//     }
+// }
+// book()
+
+// ? movie------------------------------------------------------
+
+function movie() {
+    let movies = [
+        {
+          title: "Inception",
+          director: "Christopher Nolan",
+          genre: "Science Fiction",
+          blockbuster: true,
+        },
+        {
+          title: "The Shawshank Redemption",
+          director: "Frank Darabont",
+          genre: "Drama",
+          blockbuster: false,
+        },
+        {
+          title: "The Dark Knight",
+          director: "Christopher Nolan",
+          genre: "Action",
+          blockbuster: true,
+        },
+        {
+          title: "Forrest Gump",
+          director: "Robert Zemeckis",
+          genre: "Drama",
+          blockbuster: true,
+        },
+        {
+          title: "Pulp Fiction",
+          director: "Quentin Tarantino",
+          genre: "Crime",
+          blockbuster: false,
+        },
+        {
+          title: "The Matrix",
+          director: "Lana and Lilly Wachowski",
+          genre: "Science Fiction",
+          blockbuster: true,
+        },
+        {
+          title: "The Godfather",
+          director: "Francis Ford Coppola",
+          genre: "Crime",
+          blockbuster: false,
+        },
+        {
+          title: "Avatar",
+          director: "James Cameron",
+          genre: "Action",
+          blockbuster: true,
+        },
+        {
+          title: "Titanic",
+          director: "James Cameron",
+          genre: "Romance",
+          blockbuster: true,
+        },
+        {
+          title: "The Silence of the Lambs",
+          director: "Jonathan Demme",
+          genre: "Thriller",
+          blockbuster: false,
+        },
+      ];
+
+    console.log('Bestseller--------------------------------')
+    for (i = 0; i < movies.length; i++) {
+        if (movies[i].blockbuster === true) {
+            console.log(movies[i].title);
+        }
     }
-    console.log(newName);
+    console.log('Sci-fi-----------------------------------')
+    for(i = 0; i < movies.length; i++){
+        if (movies[i].genre === 'Science Fiction'){
+            console.log(movies[i].title + ": " + movies[i].director);
+        }
+    }
+    console.log('genre and Bestseller--------------------')
+    for(i = 0; i < movies.length; i++){
+        if (movies[i].genre === 'Science Fiction'&& movies[i].blockbuster === true){
+            console.log(movies[i].title);
+        }
+    }
 }
-fruit()
-
+movie()
